dbfb7e59b7afca786746c4512a3fe2a370dc9f40
unknown <kimchoxd@gmail.com>
2024-08-14 14:02:08 -0600

    Practicum finished

--- index.js
+++ index.js
@@ -12,60 +12,62 @@ const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+  const petsEndpoint = 'https://api.hubspot.com/crm/v3/objects/pets?properties=pet_name,pet_type,pet_age';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  }
+  const params = {
+    properties: ['pet_name', 'pet_type', 'pet_age']
+  }
+  try {
+    const response = await axios.get(petsEndpoint, { headers, params });
+    /* console.log('API Response:', JSON.stringify(response.data, null, 2)); */
+    const pets = response.data.results;
+    /* console.log('pet data:', JSON.stringify(pets, null, 2)); */
+    res.render('homepage', { pets: pets });
+  } catch (error) {
+    console.error(error);
+  }
+})
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-pets', (req, res) => {
+  try {
+    res.render('updates', { pageTitle: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-pets', async (req, res) => {
+  const petsEndpoint = `https://api.hubspot.com/crm/v3/objects/pets`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  }
 
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
+  const data = {
+    properties: {
+      pet_name: req.body.pet_name,
+      pet_type: req.body.pet_type,
+      pet_age: req.body.pet_age
+    }
+  }
 
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const response = await axios.post(petsEndpoint, data, { headers });
+    console.log('API Response:', JSON.stringify(response.data, null, 2));
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
-*/
 
 
-// * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
